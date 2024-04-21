71954ec9bae4d9846780032410e396556ce4d63f
Vladimir Cheverdyuk <vladimir.cheverdyuk@altium.com>
2024-04-21 13:12:58 -0700

    - Added .NET6 project - Added x64 configurations

---
+++ D3DHost.DotNet6/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace D3DHost.DotNet6
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
